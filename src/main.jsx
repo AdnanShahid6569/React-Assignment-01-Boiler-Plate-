a74decd2148cdfad5ec3b785bd42d4a757e5cf66
AdnanShahidBakkar <adnanshahidbakkar@gmail.com>
2024-12-15 16:35:33 +0500

    React_(Boilre-plate)

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import RectDom from "react-dom"
+
+const reactroot = document.getElementById("root")
+RectDom.createRoot(reactroot).render(
+  <div>
+  <h1>ES6 old</h1>
+  <h1>React_Js new</h1>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci, omnis ea? Ad cum quibusdam nam, temporibus ducimus corrupti debitis quod doloremque voluptas repellendus! Rerum dicta neque beatae odit dolore, eaque expedita incidunt placeat atque dolorum. Ipsam vitae facere numquam quod nesciunt praesentium, atque deleniti laboriosam fugiat quos assumenda maxime saepe!</p>
+  <img src="/public/vite.svg" alt="" />
+  <img src="/src/assets/react.svg" alt="" /><br />
+  <button>React_JS</button>
+  </div>
+)
